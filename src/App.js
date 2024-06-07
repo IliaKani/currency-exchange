29737beb464875910b8afe7f3cd77a32ab09c686
Ilia Kani <kanishchev.ilia@gmail.com>
2024-06-07 12:49:39 +0200

    add API

--- src/App.js
+++ src/App.js
@@ -3,7 +3,21 @@ import { Block } from './Block';
 import './index.scss';
 
 function App() {
-  
+ 
+  const [rates, setRates] = React.useState({});
+
+  React.useEffect(() => {
+    fetch('https://api.frankfurter.app/latest')
+    .then((res) => res.json())
+    .then((json) => {
+      setRates(json.rates);
+      console.log(json.rates);
+    }).catch((err) => {
+      console.error(err);
+      alert('Failed to load exchange rates');
+    });
+  }, [])
+
   return (
     <div className="App">
       <Block value={0} currency="EUR" onChangeCurrency={(cur) => console.log(cur)} />
--- src/Block.jsx
+++ src/Block.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const defaultCurrencies = ['RUB', 'USD', 'EUR', 'GBP'];
+const defaultCurrencies = ['EUR', 'USD', 'TYR', 'GBP'];
 
 export const Block = ({ value, currency, onChangeValue, onChangeCurrency }) => (
   <div className="block">
